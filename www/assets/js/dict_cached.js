7001bf0da546d58b1f92b9b6879ac84755e24488
asoreiko <42262092+asoreiko@users.noreply.github.com>
2018-08-10 23:25:02 +0900

    Update dict_cached.js

--- www/assets/js/dict_cached.js
+++ www/assets/js/dict_cached.js
@@ -1,1 +1,167 @@
-var dataDict = {'words': {'data': [{"word":"kan","definition":"god","Kana":"かん","Jp":"神","sorting_form":[12,0,26]},{"word":"kani","definition":"crab","Kana":"かに","Jp":"カニ","sorting_form":[12,0,26,2]},{"word":"kaa","definition":"odor","Kana":"かー","Jp":"香り","sorting_form":[12,1]},{"word":"kun","definition":"come","Kana":"くん","Jp":"来る","sorting_form":[12,6,26]},{"word":"kunu","definition":"don't come","Kana":"くぬ","Jp":"来ない","sorting_form":[12,6,26,6]},{"word":"kuu","definition":"powder","Kana":"くー","Jp":"粉","sorting_form":[12,7]},{"word":"tana","definition":"rice field","Kana":"たな","Jp":"田んぼ","sorting_form":[16,0,26,0]},{"word":"taa","definition":"who","Kana":"たー","Jp":"誰","sorting_form":[16,1]},{"word":"taan","definition":"nobody","Kana":"たーん","Jp":"誰も","sorting_form":[16,1,26]},{"word":"turi","definition":"bird","Kana":"とるぃ","Jp":"鳥","sorting_form":[16,6,24,2]},{"word":"tun","definition":"wife","Kana":"とぅん","Jp":"妻","sorting_form":[16,6,26]},{"word":"tuu","definition":"ten","Kana":"とぅー","Jp":"十","sorting_form":[16,7]},{"word":"pan","definition":"foot","Kana":"ぱん","Jp":"足","sorting_form":[20,0,26]},{"word":"pana","definition":"flower","Kana":"ぱな","Jp":"花","sorting_form":[20,0,26,0]},{"word":"paa","definition":"grandmother","Kana":"ぱー","Jp":"おばあさん","sorting_form":[20,1]},{"word":"puni","definition":"bone","Kana":"ぷに","Jp":"骨","sorting_form":[20,6,26,2]},{"word":"puu","definition":"ear rice","Kana":"ぷー","Jp":"穂","sorting_form":[20,7]},{"word":"puun","definition":"ear rice too","Kana":"ぷーん","Jp":"穂も","sorting_form":[20,7,26]},{"word":"bata","definition":"stomach","Kana":"ばた","Jp":"腹","sorting_form":[21,0,16,0]},{"word":"nan'","definition":"wave","Kana":"なん","Jp":"波","sorting_form":[26,0,26]},{"word":"nin","definition":"root","Kana":"にん","Jp":"根っこ","sorting_form":[26,2,26]},{"word":"nta","definition":"soil","Kana":"んた","Jp":"土","sorting_form":[26,16,0]},{"word":"nncï","definition":"six pieces","Kana":"ん゜ーつぃ","Jp":"6個","sorting_form":[26,26,4]},{"word":"nngara","definition":"six animals","Kana":"ん゜ーがら","Jp":"6匹","sorting_form":[26,26,13,0,24,0]},{"word":"nnsïn","definition":"six pieces","Kana":"ん゜ーすぃ","Jp":"6本","sorting_form":[26,26,14,4,26]},{"word":"nnta","definition":"why","Kana":"んーた","Jp":"なぜ","sorting_form":[26,26,16,0]},{"word":"nnpïtu","definition":"six people","Kana":"ん゜ーぴすとぅ","Jp":"6人","sorting_form":[26,26,20,4,16,6]}] } }
+var dataDict = {
+	'words': {
+		'data': [{
+			"word": "kan",
+			"definition": "god",
+			"Kana": "かん",
+			"Jp": "神",
+			"sorting_form": [12, 0, 26]
+		}, {
+			"word": "kani",
+			"definition": "crab",
+			"Kana": "かに",
+			"Jp": "カニ",
+			"sorting_form": [12, 0, 26, 2]
+		}, {
+			"word": "kaa",
+			"definition": "odor",
+			"Kana": "かー",
+			"Jp": "香り",
+			"sorting_form": [12, 1]
+		}, {
+			"word": "kun",
+			"definition": "come",
+			"Kana": "くん",
+			"Jp": "来る",
+			"sorting_form": [12, 6, 26]
+		}, {
+			"word": "kunu",
+			"definition": "don't come",
+			"Kana": "くぬ",
+			"Jp": "来ない",
+			"sorting_form": [12, 6, 26, 6]
+		}, {
+			"word": "kuu",
+			"definition": "powder",
+			"Kana": "くー",
+			"Jp": "粉",
+			"sorting_form": [12, 7]
+		}, {
+			"word": "tana",
+			"definition": "rice field",
+			"Kana": "たな",
+			"Jp": "田んぼ",
+			"sorting_form": [16, 0, 26, 0]
+		}, {
+			"word": "taa",
+			"definition": "who",
+			"Kana": "たー",
+			"Jp": "誰",
+			"sorting_form": [16, 1]
+		}, {
+			"word": "taan",
+			"definition": "nobody",
+			"Kana": "たーん",
+			"Jp": "誰も",
+			"sorting_form": [16, 1, 26]
+		}, {
+			"word": "turi",
+			"definition": "bird",
+			"Kana": "とるぃ",
+			"Jp": "鳥",
+			"sorting_form": [16, 6, 24, 2]
+		}, {
+			"word": "tun",
+			"definition": "wife",
+			"Kana": "とぅん",
+			"Jp": "妻",
+			"sorting_form": [16, 6, 26]
+		}, {
+			"word": "tuu",
+			"definition": "ten",
+			"Kana": "とぅー",
+			"Jp": "十",
+			"sorting_form": [16, 7]
+		}, {
+			"word": "pan",
+			"definition": "foot",
+			"Kana": "ぱん",
+			"Jp": "足",
+			"sorting_form": [20, 0, 26]
+		}, {
+			"word": "pana",
+			"definition": "flower",
+			"Kana": "ぱな",
+			"Jp": "花",
+			"sorting_form": [20, 0, 26, 0]
+		}, {
+			"word": "paa",
+			"definition": "grandmother",
+			"Kana": "ぱー",
+			"Jp": "おばあさん",
+			"sorting_form": [20, 1]
+		}, {
+			"word": "puni",
+			"definition": "bone",
+			"Kana": "ぷに",
+			"Jp": "骨",
+			"sorting_form": [20, 6, 26, 2]
+		}, {
+			"word": "puu",
+			"definition": "ear rice",
+			"Kana": "ぷー",
+			"Jp": "穂",
+			"sorting_form": [20, 7]
+		}, {
+			"word": "puun",
+			"definition": "ear rice too",
+			"Kana": "ぷーん",
+			"Jp": "穂も",
+			"sorting_form": [20, 7, 26]
+		}, {
+			"word": "bata",
+			"definition": "stomach",
+			"Kana": "ばた",
+			"Jp": "腹",
+			"sorting_form": [21, 0, 16, 0]
+		}, {
+			"word": "nan'",
+			"definition": "wave",
+			"Kana": "なん",
+			"Jp": "波",
+			"sorting_form": [26, 0, 26]
+		}, {
+			"word": "nin",
+			"definition": "root",
+			"Kana": "にん",
+			"Jp": "根っこ",
+			"sorting_form": [26, 2, 26]
+		}, {
+			"word": "nta",
+			"definition": "soil",
+			"Kana": "んた",
+			"Jp": "土",
+			"sorting_form": [26, 16, 0]
+		}, {
+			"word": "nncï",
+			"definition": "six pieces",
+			"Kana": "ん゜ーつぃ",
+			"Jp": "6個",
+			"sorting_form": [26, 26, 4]
+		}, {
+			"word": "nngara",
+			"definition": "six animals",
+			"Kana": "ん゜ーがら",
+			"Jp": "6匹",
+			"sorting_form": [26, 26, 13, 0, 24, 0]
+		}, {
+			"word": "nnsïn",
+			"definition": "six pieces",
+			"Kana": "ん゜ーすぃ",
+			"Jp": "6本",
+			"sorting_form": [26, 26, 14, 4, 26]
+		}, {
+			"word": "nnta",
+			"definition": "why",
+			"Kana": "んーた",
+			"Jp": "なぜ",
+			"sorting_form": [26, 26, 16, 0]
+		}, {
+			"word": "nnpïtu",
+			"definition": "six people",
+			"Kana": "ん゜ーぴすとぅ",
+			"Jp": "6人",
+			"sorting_form": [26, 26, 20, 4, 16, 6]
+		}]
+	}
+}
